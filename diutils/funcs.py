53742f839afc80ea4c7947b3c0efd7551e34951d
root <root@s17115651.onlinehome-server.info>
2015-02-17 21:11:07 +0100

    ADD: getIP

--- diutils/funcs.py
+++ diutils/funcs.py
@@ -23,10 +23,19 @@ class CustomEncoder(json.JSONEncoder):
 
 
 def HttpResponseJSON(d, request=None, status=200):
-
+    """Dump a JSON dict and return as HttpResponse JSON"""
     return HttpResponse(
         json.dumps(d, cls=CustomEncoder),
         content_type='application/json',
         status=status,
         )
 
+
+def getIP(request):
+    """Get real IP client"""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
